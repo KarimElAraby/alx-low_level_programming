5aa8a1db960d6acb5107c54bc73d1411030f14d9
KarimElAraby <karimaraby007@gmail.com>
2023-07-14 05:08:40 -0700

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 /**
- *  *  * main - Entry point
- *   *   *
- *    *    * Return: Always 1 (error)
+ *fileds - filedes
  *
- *       */
-int write(int filedes,const char*buf,unsigned int nbyte);
-
+ * *buf - array
+ *
+ * nbyte -size
+ *
+ *
+ * main - Entry point
+ *
+ * Return: Always 1 (error)
+ *
+    */
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
