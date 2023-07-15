1b2a2340faee81a085fbfbde7cf9aedd92e60076
KarimElAraby <karimaraby007@gmail.com>
2023-07-14 19:07:10 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  *  *  *  *  * main - Entry point
+ *   *   *   *   *   *
+ *    *    *    *    *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch = 0;
+while (ch < 10)
+printf("%u", ch++);
+printf("\n");
+return (0);
+}
