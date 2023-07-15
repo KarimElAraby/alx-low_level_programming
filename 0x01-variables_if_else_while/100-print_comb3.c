b2e8e2da23c3215536863aaf4481b7fc20bd4f26
KarimElAraby <karimaraby007@gmail.com>
2023-07-15 14:01:29 -0700

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *  *  *  *  *  * main - Entry point
+ *   *   *   *   *   *
+ *    *    *    *    *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x = 0, y;
+while (x <= 9)
+{
+y = 0;
+while (y <= 9)
+{
+if (x != y && x < y)
+{
+putchar(x + 48);
+putchar(y + 48);
+if (x + y != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+y++;
+}
+x++;
+}
+return (0);
+}
