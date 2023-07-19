11d3a4a5f40df21ff4cc2ddb6555b3a8adfbbab7
KarimElAraby <karimaraby007@gmail.com>
2023-07-18 19:14:49 -0700

    The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@
 void print_times_table(int n)
 {
 	int i, j, prod;
-if (n <= 15 && n > 0)
+if (n <= 15 && n >= 0)
 {
 for (i = 0; i <= n; i++)
 {
@@ -25,7 +25,7 @@ for (i = 0; i <= n; i++)
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(' ');
-		_putchar(prod +48);
+		_putchar(prod + 48);
 	}
 	else if (prod <= 99)
 	{
