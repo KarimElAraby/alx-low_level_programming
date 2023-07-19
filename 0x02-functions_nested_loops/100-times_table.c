76c52bf86ae3f26c56f5be3ebd91276a6dca936b
KarimElAraby <karimaraby007@gmail.com>
2023-07-18 18:54:20 -0700

    The World looks like a multiplication-table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *  * print_times_table - prints the n times table
+ *   *
+ *   @n : input
+ *    * Example table
+ *     * 0, 0, 0, ..
+ *      * 0, 1, 2, 3, ..
+ *       *
+ */
+void print_times_table(int n)
+{
+	int i, j, prod;
+if (n <= 15 || n > 0)
+{
+for (i = 0; i <= n; i++)
+{
+	_putchar(48);
+	for (j = 1; j <= n; j++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		prod = n * n;
+	if (prod <= 9)
+	{
+		_putchar(' ');
+	}
+	else
+	{
+				_putchar((prod / 10) + 48);
+		_putchar((prod % 10) + 48);
+	}
+	}
+	_putchar('\n');
+}
+}
+}
