d648aff62125911930d1fbbc758eccbdd03cda46
KarimElAraby <karimaraby007@gmail.com>
2023-07-18 19:41:06 -0700

    In computer class

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  *  *  *  *  *  *  * main - Entry point
+ *   *   *   *   *   *   *   *
+ *    *    *    *    *    *    *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int t1 = 1, t2 = 2;
+int nextTerm = t1 + t2;
+for (i = 3; i <= 50; ++i)
+{
+	printf("1, 2, ")
+	printf("%d, ", nextTerm);
+	t1 = t2;
+	t2 = nextTerm;
+	nextTerm = t1 + t2;
+}
+return (0);
+}
