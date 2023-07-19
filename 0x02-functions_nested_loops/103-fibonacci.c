d7ad01b0f9b2d6e497b28b98e4585d920144da65
KarimElAraby <karimaraby007@gmail.com>
2023-07-19 15:41:27 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *  *  *  *  *  *  *  *  * main - Entry point
+ *   *   *   *   *   *   *   *   *
+ *    *    *    *    *    *    *    *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+unsigned long t1 = 0, t2 = 1, nextTerm;
+float total_sum;
+while (1 < 2)
+{
+nextTerm = t1 + t2;
+if (nextTerm > 4000000)
+	break;
+if ((nextTerm % 2) == 0)
+	total_sum += nextTerm;
+	t1 = t2;
+	t2 = nextTerm;
+}
+printf("%.0f\n", total_sum);
+return (0);
+}
