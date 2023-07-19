2f4d8d8456d9c802415a981bfec087a03e0f86be
KarimElAraby <karimaraby007@gmail.com>
2023-07-18 19:51:45 -0700

    In computer class

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,11 +8,11 @@ int main(void)
 {
 int i;
 int t1 = 1, t2 = 2;
-long int nextTerm = t1 + t2;
+unsigned long nextTerm = t1 + t2;
 	printf("1, 2, ");
 for (i = 3; i <= 50; ++i)
 {
-	printf("%ld, ", nextTerm);
+	printf("%lu, ", nextTerm);
 	t1 = t2;
 	t2 = nextTerm;
 	nextTerm = t1 + t2;
