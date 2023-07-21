45d0bfe9712f3f3a853e826c7050ce23117288c0
KarimElAraby <karimaraby007@gmail.com>
2023-07-21 09:00:49 -0700

    I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,9 +12,9 @@ if (n <= 0)
 	_putchar('\n');
 else
 {
-for (i = 1; i <= n; i++)
+for (i = 1; i < n; i++)
 {
-for (j = 1; j <= i; j++)
+for (j = 1; j < i; j++)
 	_putchar(' ');
 _putchar('\\');
 _putchar('\n');
