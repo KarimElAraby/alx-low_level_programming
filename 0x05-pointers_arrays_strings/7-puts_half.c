5e4cded16fd357bc97ab9875bd7ddd7b3501c561
KarimElAraby <karimaraby007@gmail.com>
2023-07-24 18:10:34 -0700

    Winning is only half of it

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+include "main.h"
+/**
+ *  *  *puts_half - returns the length of a string
+ *   *   *   *
+ *    *    *    *@str : pointer
+ *     *     *     *
+ *      *      *Return: void
+ */
+void puts_half(char *str);
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+	;
+
+if (i % 2 = 0)
+{
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
+else 
+{
+for (i = (i - 1) / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
