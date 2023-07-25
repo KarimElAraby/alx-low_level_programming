6c2466e20caa38a0ec63ee331517d64212a16d87
KarimElAraby <karimaraby007@gmail.com>
2023-07-24 18:37:41 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  *  *_strcpy - swaps the values of two integers
+ *   *   *
+ *    *    *@dest : 1st pointer
+ *     *     *@src : 2nd pointer
+ *      *      *
+ *      Return: value the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = - 1;
+do {
+i++;
+dest[i] = src[i];
+} while (src[i] != '\0');
+return (dest);
+}
