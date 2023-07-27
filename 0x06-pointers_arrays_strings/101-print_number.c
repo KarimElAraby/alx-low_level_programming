b97da349dc3520d7595294c7d2a6e5d63dc0314e
KarimElAraby <karimaraby007@gmail.com>
2023-07-27 15:19:56 -0700

    Numbers have life

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  *  *  *  *  *print_numberr - swaps the values of two integers
+ *   *   *   *   *   *
+ *    *     *     *     *     *@n : 2nd pointer
+ *     *      *      *      *      *
+ *      *        *       *      Return: value the pointer to dest
+ */
+void print_number(int n)
+{
+unsigned int n1;
+n1 = n;
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
+}
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0')
+}
