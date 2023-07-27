767e707ab7845b48ddf45a03faf7fe7028da2a97
KarimElAraby <karimaraby007@gmail.com>
2023-07-27 14:04:19 -0700

    Expect the best

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  *  *  *  *isLower - swaps the values of two integers
+ *    *    *    *    *@c : 1st pointer
+ *        *       *      Return: return 1 is true , 0 if false
+ */
+int isLower(char c)
+{
+return (c >= 97 && c <= 122);
+}
+/**
+ *  *  *  *  *  *isDelimiter - swaps the values of two integers
+ *   *    *    *    *    *@c : 1st pointer
+ *    *        *       *      Return: return 1 is true , 0 if false
+ */
+int isDelimiter(char c)
+{
+int i;
+char delimiter[] = " \t\n,;.!?\"(){}"
+	for (i = 0; i < 13; i++)
+		if (c == delimiter[i])
+			return (1);
+return (0);
+}
+
+/**
+ *  *  *  *  *cap_string - swaps the values of two integers
+ *       *@s : most number of bytes from src
+ *        *       *      Return: return string to captalize word
+ */
+
+char *cap_string(char *s)
+{
+char *ptr = s;
+int x = 1;
+while (*s)
+{
+	if (isDelimiter[*s])
+		x = 1;
+	else if (isLower[*s] && x)
+	{
+		*s -= 32;
+		x = 0;
+	}
+	else
+		x = 0;
+s++;
+}
+return (ptr);
+}
