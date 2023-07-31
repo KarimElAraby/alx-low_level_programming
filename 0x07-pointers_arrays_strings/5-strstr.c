50d5d745d74eb8e5b07b8c478b6ee4ad56eee520
KarimElAraby <karimaraby007@gmail.com>
2023-07-31 16:37:31 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  *  *  *  *  *  *  *_strstr - fills memory with a constnat byte
+ *   *   *   *   *   *   *   *
+ *    *    *    *    *    *    *    *@haystack : pointer to put a constant
+ *     *     *     *     *     *     *     *@needle : constant
+ *      *      *       *       *       *      Return: pointer s
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *one = haystack;
+char *two = needle;
+while (*one == *two && != '\0')
+{
+one++;
+two++;
+}
+if (*two == '\0')
+return (haystack);
+}
+return ('\0');
+}
