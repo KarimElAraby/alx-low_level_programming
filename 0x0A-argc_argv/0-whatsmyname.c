57e4ef98dc5ba6fde5f7bc3192ad15e6d6e1068f
KarimElAraby <karimaraby007@gmail.com>
2023-08-04 11:40:11 -0700

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  *  *  * main - Entry point
+ *   *   *   *
+ *   *@argc : numbers of arguments
+ *   *@argv : variables of arguments
+ *    *    *    * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
