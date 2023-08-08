d1a183866080a3de5c7562a7f8383c2b17e32e46
KarimElAraby <karimaraby007@gmail.com>
2023-08-08 10:12:06 -0700

    Float like a butterfly

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  *  *  *  *create_array - to create an array
+ *   *   *   *   *
+ *    *    *    *    *@size : size
+ *     *     *     *     *@c : char
+ *       *      *      *      *
+ *        *       *      Return: 0 success . null fail
+ */
+char *create_array(unsigned int size, char c)
+{
+char *p = malloc(size);
+
+if (size == 0 || p == 0)
+	return (0);
+
+while (size--)
+	p[size] = c;
+
+return (n);
+}
