1b0f16cc930988d2e6214b216e99b76789283c89
KarimElAraby <karimaraby007@gmail.com>
2023-08-08 15:18:33 -0700

    It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  *  *  *  *  *  *free_grid - 2 dimn array
+ *   *   *   *   *   *   *
+ *    *    *    *    *    *    *@grid : 2 dimn array
+ *     *     *     *     *     *     *@height : height
+ *      *      *       *      *      *      *
+ *       *       *        *       *      Return: 0 success . null fail
+ *        */
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (; i < height; i++)
+	free(grid[i]);
+free(grid);
+}
