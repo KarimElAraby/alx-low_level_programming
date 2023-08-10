07b0e8689c498fc62c56e602bec595942faa4306
KarimElAraby <karimaraby007@gmail.com>
2023-08-10 15:44:33 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  *  *  *  *  *malloc_checked - to allocates memory using
+ *     *     *     *     *     *@b : numbers of bytes
+ *       *        *       *      Return: to pointer success . 98 if fails
+ */
+void *malloc_checked(unsigned int b)
+{
+int *p;
+p = mallock(b)
+
+	if (p == 0)
+		exit(98);
+
+return (p);
+}
