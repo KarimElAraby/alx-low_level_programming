fdfa02b175dc430b0dc8cb40ab42e90c03fc349d
KarimElAraby <karimaraby007@gmail.com>
2023-08-16 10:09:40 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_name - print name
+ * @name : the name
+ * @f : the printing function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
