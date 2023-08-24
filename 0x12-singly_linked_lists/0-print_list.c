4c670b0a220984ae7de690ec23503bcb284418f6
KarimElAraby <karimaraby007@gmail.com>
2023-08-24 14:13:22 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -22,13 +22,13 @@ return (i);
  */
 size_t print_list(const list_t *h)
 {
-size_t j = 0;
+size_t i = 0;
 
 while (h)
 {
 	printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 	h = h->next;
-	j++;
+	i++;
 }
-return (j);
+return (i);
 }
