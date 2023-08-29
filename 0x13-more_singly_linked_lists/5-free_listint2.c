9ce2569b3757fe2caf2af985429c1e2f63a1e84b
KarimElAraby <karimaraby007@gmail.com>
2023-08-29 07:16:37 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *  *  *  * free_listint2 - calculate number of node
+ *   *   *   * @head : pointer to the head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	*head = NULL;
+}
