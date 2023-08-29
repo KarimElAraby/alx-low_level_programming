f6bc158b1fa33b7a20653670e1bcaea3c73fb870
KarimElAraby <karimaraby007@gmail.com>
2023-08-29 07:23:21 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -17,9 +17,9 @@ void free_listint2(listint_t **head)
 
 	while (current != NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		next = current;
+		current = current->next;
+		free(next);
 	}
 
 	*head = NULL;
