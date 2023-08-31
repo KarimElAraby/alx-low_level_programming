48b27dbf0c46bb7a48948318fff7493889808f9b
KarimElAraby <karimaraby007@gmail.com>
2023-08-31 16:55:28 -0700

    0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+
 int _putchar(char s);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
