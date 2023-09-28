bcb48c9a3ffa5d49744c5a064b79988f9e7a42cb
KarimElAraby <karimaraby007@gmail.com>
2023-09-28 16:19:45 -0700

    10

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_bit - gets bit in index
+ * @n: number
+ * @index: the bit
+ * Return: the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= sizeof(n) * 8)
+	return (-1);
+return (n >> index & 1);
+}
